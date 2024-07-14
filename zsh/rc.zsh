9fd3832981597e62ac7a793bf587640cc504649a
Jakub Wadrzyk <92339252+Shikoqu@users.noreply.github.com>
2024-07-15 00:20:05 +0200

    added starship prompt with catppuccin theme

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -1,1 +1,6 @@
 #!/usr/bin/env zsh
+
+# Initialize starship
+[[ -f ~/.dotfiles/zsh/starship/def-distro.zsh ]] && source ~/.dotfiles/zsh/starship/def-distro.zsh
+eval "$(starship init zsh)"
+export STARSHIP_CONFIG=~/.dotfiles/zsh/starship/config.toml
